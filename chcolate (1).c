45b30c691b2a22f1ccbe88713b69974ce098783c
kuMunasinghe <kasun.u.munasinghe@gmail.com>
2021-11-27 19:16:54 +0530

    This is the committed files

---
+++ chcolate (1).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+   int size[3][3];
+  
+   int i, j,amount;
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("Enter value for chocolatesize[%d][%d]:", i, j);
+         scanf("%d", &chocolatesize[i][j]);
+      amount=+200;
+
+      }
+   }
+   return 0;
+}
---
+++ chcolate (2).c
@@ -0,0 +1,6 @@
+#include <stduio.h>
+
+int main(){
+    int i,j;
+    for(i=0;i<5;i++)
+}
